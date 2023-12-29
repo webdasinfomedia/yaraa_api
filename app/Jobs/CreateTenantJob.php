b8bfb7d1cf0f197c7bd4073d06f1439977d59615
Priyal <priyal@dasinfomedia.com>
2023-12-29 15:47:08 +0530

    Update CreateTenantJob.php

--- app/Jobs/CreateTenantJob.php
+++ app/Jobs/CreateTenantJob.php
@@ -61,7 +61,9 @@ class CreateTenantJob extends Job
         }
 
         //3.create database for tenant
-        $client = new \MongoDB\Client("mongodb://admin:" . env('DB_PASSWORD') . "@127.0.0.1:27017");
+        $connectionString = "mongodb://" . env('DB_USERNAME') . ":" . env('DB_PASSWORD') . "@" . env('DB_HOST') . ":" . env('DB_PORT') . "";
+        // $client = new \MongoDB\Client("mongodb://admin:" . env('DB_PASSWORD') . "@127.0.0.1:27017");
+        $client = new \MongoDB\Client($connectionString);
         $db = $client->{$tenantDatabase};
         $db->createCollection('test');
 
