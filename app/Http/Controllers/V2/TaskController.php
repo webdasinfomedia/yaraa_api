afa2cfe75735ad8f31c7bfd4efd2dff039a32afa
Dharadasinfomedia <dhara@dasinfomedia.com>
2024-02-23 15:26:39 +0530

    Add remaining columns in the TaskImport API.

--- app/Http/Controllers/V2/TaskController.php
+++ app/Http/Controllers/V2/TaskController.php
@@ -41,32 +41,55 @@ class TaskController extends Controller
                 $file = fopen($csvFile, 'r');
 
                 fgetcsv($file); // to skip first row
-                while (($data = fgetcsv($file, 100, ",")) !== FALSE) {
+                while (($data = fgetcsv($file, 10000, ",")) !== FALSE) {
                     $task = Task::create([
-                        'name' => $data[0],
-                        'priority' => $data[1],
-                        'visibility' => "private",
+                        'project_id' => $data[0],
+                        'name' => $data[1],
+                        'description' => $data[2],
+                        'visibility' => $data[3],
                         'recurrence' => "no",
                         'created_by' => Auth::user()->id,
                         'assigned_by' => Auth::user()->email,
                         'reminder' => false,
-                        'start_date' => null,
                         'status' => "pending",
+                        // 'priority' => $data[7],
+                        'priority' => "high",
                     ]);
 
+                    // Check if project is exists or not
+                    $validation = Validator::make(['project_id' => $task->project_id], [
+                        'project_id' => 'filled|exists:projects,_id',
+                    ]);
+                    if ($validation->fails()) {
+                        $task->project_id = null;
+                    }
+
+                    // For start_date
+                    $start_date = $data[4];
+                    if ($start_date) {
+                        $task->start_date = $start_date;
+                    } else {
+                        $task->start_date = null;
+                    }
+
+                    // For due_date
+                    $end_date = $data[5];
+                    if ($end_date) {
+                        $task->due_date = $end_date;
+                    } else {
+                        $currentDate = Carbon::now();
+                        $dueDate = $currentDate->addDay();
+                        $formattedDueDate = $dueDate->format('Y-m-d h:i A');
+                        $date = Carbon::createFromFormat('Y-m-d H:i A', $formattedDueDate, getUserTimezone());
+                        $date->setTimezone('UTC');
+                        $task->due_date = $date;
+                    }
+
                     //add assign members to task
-                    $assignee = array_filter(explode(',', $data[2]));
+                    $assignee = array_filter(explode(',', $data[6]));
                     $assignee[] = auth()->user()->email; //keep task owner as member
                     $task->sync($assignee, [], 'assignedTo')->syncProjectMember();
                     $task->skipUser(auth()->id())->sendTaskMail(false);
-
-                    /** convert due_date from user TZ to UTC TZ and save **/
-                    $currentDate = Carbon::now();
-                    $dueDate = $currentDate->addDay();
-                    $formattedDueDate = $dueDate->format('Y-m-d h:i A');
-                    $date = Carbon::createFromFormat('Y-m-d H:i A', $formattedDueDate, getUserTimezone());
-                    $date->setTimezone('UTC');
-                    $task->due_date = $date;
 
                     $task->save();
                 }
