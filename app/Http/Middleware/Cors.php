ba8b15ede466b37dc4eb0c392a1658c2ae9be92e
Priyal <priyal@dasinfomedia.com>
2024-03-15 15:42:33 +0530

    Update Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -26,7 +26,7 @@ class Cors
             'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age' => '86400',
-            'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With, Access-Control-Request-Method'
+            'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With'
         ];
 
         if ($request->isMethod('OPTIONS')) {
